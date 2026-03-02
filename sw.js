96e1b8a56f1092e93590b1a477cd9591b23f5f9a
Cohen6526 <cohenteschke@gmail.com>
2026-03-02 12:21:26 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const version = 'v124';  // change this everytime you update the service worker
+                          // to force the browser to also update it.
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'script.js',
+        'icons/favicon.png',
+        'icons/icon512_maskable.png',
+        'icons/icon512_rounded.png',
+        'sfx/dingding.mp3',
+        'screenshots/screenshot1.png',
+        'screenshots/screenshot2.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
